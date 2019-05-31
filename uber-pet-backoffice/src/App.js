cadd8d3a7a3746ee8f641b12d1cc3673f4db5d84
Federico Longhi <federico.rlonghi@gmail.com>
2019-05-31 17:39:38 -0300

    agrego login page

--- uber-pet-backoffice/src/App.js
+++ uber-pet-backoffice/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleTable from './components/SimpleTable';
+import AppTabs from './components/AppTabs';
+import SignInPage from './components/SignInPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SignInPage />
     </div>
   );
 }
